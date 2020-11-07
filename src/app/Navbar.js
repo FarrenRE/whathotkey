f6421e39c9780f280e5e0180c3a34473ecf31f3f
Farren Esman <farren.esman@gmail.com>
2020-11-07 15:41:51 +1100

    remove posts

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -5,12 +5,11 @@ export const Navbar = () => {
   return (
     <nav>
       <section>
-        <h1>Redux Essentials Example</h1>
+        <h1>What Hotkey?</h1>
 
         <div className="navContent">
           <div className="navLinks">
-            <Link to="/">Posts</Link>
-            <Link to="/keys">Keys</Link>
+            <Link to="/">Home</Link>
           </div>
         </div>
       </section>
