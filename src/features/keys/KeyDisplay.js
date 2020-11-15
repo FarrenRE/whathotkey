9393b3b0985cde73cf1b2b4862a42a770d3a9052
Farren Esman <farren.esman@gmail.com>
2020-11-15 14:21:34 +1100

    remove logs

--- src/features/keys/KeyDisplay.js
+++ src/features/keys/KeyDisplay.js
@@ -4,23 +4,19 @@ import { useSelector } from 'react-redux'
 import styles from './KeyDisplay.module.scss'
 
 export const KeyDisplay = () => {
+  const debug = false;
   const activeKey = useSelector(state => state.keys.activeKey)
   const profileHotkeys = useSelector(state => state.keys.profile.hotkeys)
 
   const renderHotkeyDescription = () => {
     let hotkeyDescription = ''
     for( let hotkey of profileHotkeys ) {
-      console.log('renderHotkeyDescription, hotkey:')
-      console.log(hotkey)
-      console.log('activeKey:')
-      console.log(activeKey)
       if( 
         activeKey.key.toUpperCase() === hotkey.key.toUpperCase()
         && activeKey.ctrlKey === hotkey.ctrlKey
         && activeKey.altKey === hotkey.altKey
         && activeKey.shiftKey === hotkey.shiftKey
       ) {
-        console.log('true')
         hotkeyDescription = hotkey.description
         break
       }
