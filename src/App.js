ed39376ddc0d9776f3f57130497aa4d215943d04
Farren Esman <farren.esman@gmail.com>
2021-01-05 14:28:28 +1100

    remove comments and import

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import {
 import { Navbar } from './app/Navbar'
 import { KeyDisplay } from './features/keys/KeyDisplay'
 import { KeyInput } from './features/keys/KeyInput'
-import { AddKeybindForm } from './features/keys/AddKeybindForm'
 
 function App() {
   return (
@@ -24,11 +23,9 @@ function App() {
               <React.Fragment>
                 <KeyDisplay />
                 <KeyInput />
-                {/* <AddKeybindForm /> */}
               </React.Fragment>
             )}
           />
-          {/* <Route exact path="/posts/:postId" component={SinglePostPage} /> */}
           <Redirect to="/" />
         </Switch>
       </div>
